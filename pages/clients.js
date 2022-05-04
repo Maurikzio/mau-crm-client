c50cf706451282c1a6fca5169ea71c26174c2151
Mauricio.M <m.morocho1196@gmail.com>
2022-05-04 10:06:32 -0500

    Added initial clients page

---
+++ pages/clients.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+const Clients = () => {
+  return (
+    <div>
+      <Layout>Clients</Layout>
+    </div>
+  );
+};
+
+export default Clients;

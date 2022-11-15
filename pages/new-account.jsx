e019c09d7042f9c3450f68cdbe1a8c66cf09492d
Mauricio.M <m.morocho1196@gmail.com>
2022-11-15 06:04:50 +0200

    Added formik, yup and validation for new account

--- pages/new-account.jsx
+++ pages/new-account.jsx
@@ -1,29 +1,56 @@
 import Layout from '../components/Layout'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
 
 /*
   private page it will be used for users only
 */
 export default function NewAccount () {
+  // form validation
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      lastname: '',
+      email: '',
+      password: ''
+    },
+    validationSchema: Yup.object({
+      name: Yup.string().required('Name is required'),
+      lastname: Yup.string().required('Last name is required'),
+      email: Yup.string().email('Email is not valid').required('Email is required'),
+      password: Yup.string().required('Password is required').min(6, 'Password must be at least 6 characters')
+    }),
+    onSubmit: (values) => {
+      console.log(values)
+      console.log('values: ', values)
+    }
+  })
+
   return (
     <Layout>
       <h1 className='text-center text-2xl text-white font-light'>Create New Account</h1>
       <div className='flex justify-center mt-5'>
         <div className='w-full max-w-sm'>
-          <form className='bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4'>
-            <div className='mb-4'>
+          {/* eslint-disable-next-line react/jsx-handler-names */}
+          <form className='bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4' onSubmit={formik.handleSubmit}>
+            <div className='mb-2 pb-4 relative'>
               <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='name'>
                 Name
               </label>
               <input
                 type='text'
                 id='name'
-                name='email'
+                name='name'
                 className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-gray-700'
                 autoComplete='off'
                 placeholder='Name'
+                value={formik.values.name}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
               />
+              {(formik.errors.name && formik.touched.name) ? <p className='text-xs text-red-600 absolute bottom-0 left-0'>{formik.errors.name}</p> : null}
             </div>
-            <div className='mb-4'>
+            <div className='mb-2 pb-4 relative'>
               <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='lastname'>
                 Last Name
               </label>
@@ -34,9 +61,13 @@ export default function NewAccount () {
                 className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-gray-700'
                 autoComplete='off'
                 placeholder='Last name'
+                value={formik.values.lastname}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
               />
+              {(formik.errors.lastname && formik.touched.lastname) ? <p className='text-xs text-red-600 absolute bottom-0 left-0'>{formik.errors.lastname}</p> : null}
             </div>
-            <div className='mb-4'>
+            <div className='mb-2 pb-4 relative'>
               <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='email'>
                 Email
               </label>
@@ -46,10 +77,14 @@ export default function NewAccount () {
                 className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-gray-700'
                 autoComplete='off'
                 placeholder='Email'
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
               />
+              {(formik.errors.email && formik.touched.email) ? <p className='text-xs text-red-600 absolute bottom-0 left-0'>{formik.errors.email}</p> : null}
             </div>
 
-            <div>
+            <div className='pb-4 relative'>
               <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='password'>
                 Password
               </label>
@@ -60,7 +95,11 @@ export default function NewAccount () {
                 autoComplete='off'
                 placeholder='Password'
                 type='password'
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
               />
+              {(formik.errors.password && formik.touched.password) ? <p className='text-xs text-red-600 absolute bottom-0 left-0'>{formik.errors.password}</p> : null}
             </div>
 
             <input
